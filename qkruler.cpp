5d3ad31c9f5be901fade83c7f8ce011da72f8067
ZhuJin Li <lizhujin@outlook.com>
2019-06-09 03:47:16 +0800

    Draw basic elements

--- qkruler.cpp
+++ qkruler.cpp
@@ -10,6 +10,8 @@
 QkRuler::QkRuler(QWidget *parent)
     : QWidget(parent, Qt::FramelessWindowHint)
 {
+    setAttribute(Qt::WA_TranslucentBackground);
+
     _initTray();
 
     m_geoCalc.setRulerSize(QSize{400, 100});
@@ -75,7 +77,10 @@ void QkRuler::_updateMask()
     QPainter painter(&mask);
     painter.setTransform(m_geoCalc.getTransform());
     painter.setBrush(Qt::color1);
-    painter.drawRect(QRect{0, 0, m_geoCalc.getRulerSize().width(), m_geoCalc.getRulerSize().height()});
+
+    QRect rect(0, 0, m_geoCalc.getRulerSize().width(), m_geoCalc.getRulerSize().height());
+    rect = rect.marginsAdded(QMargins(1, 1, 1, 1)); // Expand for AA
+    painter.drawRect(rect);
 
     setMask(mask);
 }
@@ -83,7 +88,45 @@ void QkRuler::_updateMask()
 
 void QkRuler::paintEvent(QPaintEvent *)
 {
+    QSize rulerSize = m_geoCalc.getRulerSize();
+    int w = rulerSize.width();
+    int h = rulerSize.height();
+
     QPainter painter(this);
-    painter.setBrush(QColor(0xff, 0, 0));
-    painter.drawRect(QRect{0, 0, this->geometry().width(), this->geometry().height()});
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setTransform(m_geoCalc.getTransform());
+    painter.setBrush(QColor(0xff, 0xff, 0xff, 0xc0));
+    painter.drawRect(QRect{0, 0, w, h});
+
+    QFont font = painter.font();
+    font.setPixelSize(11);
+    painter.setFont(font);
+
+    // Ticks
+    painter.setPen(Qt::black);
+    for (int tick = 0; tick < w; tick++) {
+        if (tick % 2 == 0) {
+            int len = tick % 100 == 0 ? 15 : tick % 10 == 0 ? 10 : 5;
+            painter.drawLine(tick, 0, tick, len);
+            painter.drawLine(tick, h,tick, h - len);
+        }
+    }
+
+    // Labels
+    for (int tick = 0; tick < w; tick++) {
+        if (tick % 100 == 0) {
+            QString label = QString::number(tick * devicePixelRatio());
+
+            QRect upperRect(tick, 15, 100, 100);
+            painter.drawText(upperRect, Qt::AlignLeft | Qt::AlignTop, label);
+
+            QRect lowerRect(tick, h - 15 - 100, 100, 100);
+            painter.drawText(lowerRect, Qt::AlignLeft | Qt::AlignBottom, label);
+        }
+    }
+
+    // Info
+    QRect infoRect(15, 0, 100, h);
+    QString infoText = QString::number(w * devicePixelRatio()) + " px";
+    painter.drawText(infoRect, Qt::AlignLeft | Qt::AlignVCenter, infoText);
 }
